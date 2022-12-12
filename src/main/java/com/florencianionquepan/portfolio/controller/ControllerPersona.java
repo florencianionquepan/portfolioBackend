162b7ba487332f53e153690d72ddba8e4af1cf67
FlorÑonquepan <florencianionquepan@outlook.com>
2022-12-12 10:54:37 -0300

    Se especifican múltiples orígenes en @CrossOrigin

--- src/main/java/com/florencianionquepan/portfolio/controller/ControllerPersona.java
+++ src/main/java/com/florencianionquepan/portfolio/controller/ControllerPersona.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins="https://portfolio-b618d.web.app")
+@CrossOrigin(origins= {"https://portfolio-b618d.web.app", "https://florencianionquepan.com"})
 public class ControllerPersona {
     
     @Autowired
